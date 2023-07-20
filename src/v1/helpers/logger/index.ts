17677fee0096d4c706ffad2e6dcb7774e769075e
gary003 <gary.johnson003@gmail.com>
2023-07-20 16:56:22 +0200

    update logger for production

--- src/v1/helpers/logger/index.ts
+++ src/v1/helpers/logger/index.ts
@@ -1,10 +1,30 @@
-import winston from "winston"
+import { createLogger, format, transports } from "winston"
+const { combine, timestamp, printf } = format
 
-// Create a Winston logger instance
-const logger = winston.createLogger({
-  level: "warn",
-  format: winston.format.simple(),
-  transports: [new winston.transports.Console(), new winston.transports.File({ filename: "logs.log" })],
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`
 })
+
+const logger = createLogger({
+  format: combine(timestamp(), myFormat),
+  exceptionHandlers: [new transports.File({ filename: "exceptions.log" })],
+})
+
+// Call rejections.handle with a transport to handle rejections
+logger.rejections.handle(new transports.File({ filename: "rejections.log" }))
+
+if (process.env.NODE_ENV === "production") {
+  logger.level = "error"
+  logger.add(
+    new transports.File({
+      level: "error",
+      filename: "error.log",
+      handleRejections: true,
+    })
+  )
+} else {
+  logger.level = process.env.LOGLEVEL || "debug"
+  logger.add(new transports.Console())
+}
 
 export default logger
