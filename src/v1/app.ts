ea8ec4177f0750814a2674fcaff280d4b7a86455
gary003 <gary.johnson003@gmail.com>
2024-03-25 00:48:56 +0100

    update app.ts with middlewares

--- src/v1/app.ts
+++ src/v1/app.ts
@@ -3,18 +3,29 @@ require("dotenv").config()
 import express from "express"
 import userRoute from "./routes/user/index"
 
+import { handleError } from "./middlewares/errorHandler"
+import { handleNotFound } from "./middlewares/handleNotFound"
+
 import swaggerUi from "swagger-ui-express"
 import swaggerJson from "./helpers/swagger/config"
 
+import helmet from "helmet"
 import cors from "cors"
 
 const app = express()
 
 app.use("/apiDoc", swaggerUi.serve, swaggerUi.setup(swaggerJson))
 
+app.use(helmet())
 app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/user", userRoute)
+
+// Not found handler
+app.use(handleNotFound)
+
+// Error handler middleware
+app.use(handleError)
 
 export default app
---
+++ src/v1/middlewares/errorHandler.ts
@@ -0,0 +1,29 @@
+import express from "express"
+
+export function handleError(err: Error, req: express.Request, res: express.Response, next: express.NextFunction) {
+  console.error(err.stack) // Log the error for debugging
+
+  // Set default status code to 500 (Internal Server Error)
+  let statusCode = 500
+
+  // Map specific error types to appropriate status codes
+  if (err.name === "ValidationError") {
+    statusCode = 400 // Bad request for validation errors
+  } else if (err.name === "UnauthorizedError") {
+    statusCode = 401 // Unauthorized for authentication errors
+  } else if (err.name === "ForbiddenError") {
+    statusCode = 403 // Forbidden for authorization errors
+  }
+
+  // Craft a generic error response with optional details based on environment
+  const errorResponse = {
+    message: "Internal Server Error", // Default message
+    error: "",
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    errorResponse.error = err.message // Include more details in development
+  }
+
+  res.status(statusCode).json(errorResponse)
+}
