ef7ef0a2fe550cc379410a67dc8c9c4b993251c5
gary003 <gary.johnson003@gmail.com>
2024-07-07 19:37:27 +0700

    fix sql creation(user table)

--- src/v1/infrastructure/persistance/sqlCreation.sql
+++ src/v1/infrastructure/persistance/sqlCreation.sql
@@ -1,12 +1,18 @@
 drop table if exists wallet;
 drop table if exists user;
 
-insert into user(userId, firstname, lastname, clubId) 
+create table user(
+  userId varchar(50) primary key,
+  firstname varchar(50),
+  lastname varchar(50)
+);
+
+insert into user(userId, firstname, lastname) 
   values 
-  ("22ef5564-0234-11ed-b939-0242ac120002", "Carol", "Peletier", "77ef5564-0234-11ed-b939-0242ac120002"),
-  ("35269564-0234-11ed-b939-0242ac120002", "Beth", "Greene", "77ef5564-0234-11ed-b939-0242ac120002"),
-  ("14523564-0234-11ed-b939-0242ac120002", "Glen", "Rhee", null),
-  ("68965564-0234-11ed-b939-0242ac120002", "Rick", "Grimes", null);
+  ("22ef5564-0234-11ed-b939-0242ac120002", "Carol", "Peletier"),
+  ("35269564-0234-11ed-b939-0242ac120002", "Beth", "Greene"),
+  ("14523564-0234-11ed-b939-0242ac120002", "Glen", "Rhee"),
+  ("68965564-0234-11ed-b939-0242ac120002", "Rick", "Grimes");
 
 create table wallet(
   walletId varchar(50) primary key,
