99106dcff09c4f2ad9f366458383e5c13e6a1d99
gary003 <gary.johnson003@gmail.com>
2025-08-17 21:37:59 +0700

    use of docker compose(not docker-compose)

--- src/v1/infrastructure/observability/instrumentation.ts
+++ src/v1/infrastructure/observability/instrumentation.ts
@@ -1,7 +1,7 @@
 /*instrumentation.ts*/
 import { NodeSDK } from '@opentelemetry/sdk-node'
 // import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node'
-import { PeriodicExportingMetricReader, ConsoleMetricExporter } from '@opentelemetry/sdk-metrics'
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics'
 import { resourceFromAttributes } from '@opentelemetry/resources'
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions'
 
